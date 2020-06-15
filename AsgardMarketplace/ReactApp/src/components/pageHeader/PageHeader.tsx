28faaeec6423965bba314d6eaf34ced0d8312904
Joris Medeisis <joris.medeisis@gmail.com>
2020-06-16 01:31:50 +0300

    initialize project add frontend structure add marketplace component

---
+++ AsgardMarketplace/ReactApp/src/components/pageHeader/PageHeader.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+interface IPageHeaderProps {
+    title: string,
+    subtitle: string
+}
+
+const PageHeader = ({ title, subtitle } : IPageHeaderProps) => (
+    <div>
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+    </div>
+);
+
+export default PageHeader;
---
+++ AsgardMarketplace/ReactApp/src/constant.ts
@@ -0,0 +1,9 @@
+﻿const constant = {
+    MARKETPLACE: {
+        HEADER_TITLE: "Welcome to the AsgardMarketplace.gg",
+        HEADER_SUBTITLE: "Abundance of unique items with good prices!"
+    }
+    
+}
+
+export default constant
